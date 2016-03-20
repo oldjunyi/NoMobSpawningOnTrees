d05eb26b4691c09b35ee8a2e197fdf7c2e3ad38a
oldjunyi <archon.jk@hotmail.com>
2016-03-20 11:34:28 +0800

    new tag: position

--- src/main/java/com/mmyzd/nmsot/SpawningEntry.java
+++ src/main/java/com/mmyzd/nmsot/SpawningEntry.java
@@ -9,15 +9,15 @@ import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 public class SpawningEntry {
 	
 	public BlockPos pos;
-	public int damage;
+	public int x, y, z, damage;
 	public World world;
 	public Block block;
 	public Entity entity;
 	
 	public void init(LivingSpawnEvent.CheckSpawn event) {
-		int x = (int)Math.floor(event.x);
-		int y = (int)event.y - 1;
-		int z = (int)Math.floor(event.z);
+		x = (int)Math.floor(event.x);
+		y = (int)event.y - 1;
+		z = (int)Math.floor(event.z);
 		pos = new BlockPos(x, y, z);
 		world  = event.world;
 		block  = world.getBlockState(pos).getBlock();
---
+++ src/main/java/com/mmyzd/nmsot/rule/RulePosition.java
@@ -0,0 +1,47 @@
+package com.mmyzd.nmsot.rule;
+
+import java.util.LinkedList;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.mmyzd.nmsot.SpawningEntry;
+
+public class RulePosition extends Rule {
+	
+	int[] lhs = new int[3];
+	int[] rhs = new int[3];
+	
+	public RulePosition(LinkedList<Character> s) throws Exception {
+		RuleSet.nextPart(s);
+		String[] pos = RuleSet.getToken(s).split(",");
+		if (pos.length != 3) throw new Exception("Invalid coordinate");
+		for (int i = 0; i < 3; i++) {
+			lhs[i] = 0;
+			rhs[i] = 99999999;
+			if (pos[i].equals("*")) continue;
+			String[] u = pos[i].split("-");
+			if (u.length < 1 || u.length > 2) throw new Exception("Invalid coordinate");
+			for (int j = 0; j < u.length; j++)
+				if (!StringUtils.isNumeric(u[j]) || u[j].length() > 18)
+					throw new Exception("Invalid coordinate");
+			lhs[i] = (int)Math.min(Long.parseLong(u[0]), rhs[i]);
+			if (u.length == 1) rhs[i] = lhs[i];
+			if (u.length == 2) rhs[i] = (int)Math.min(Long.parseLong(u[1]), rhs[i]);
+			if (lhs[i] > rhs[i]) {
+				int at = lhs[i];
+				lhs[i] = rhs[i];
+				rhs[i] = at;
+			}
+		}
+		lhs[1]--;
+		rhs[1]--;
+	}
+
+	@Override
+	public boolean apply(SpawningEntry entry) {
+		return entry.x >= lhs[0] && entry.x <= rhs[0]
+			&& entry.y >= lhs[1] && entry.y <= rhs[1]
+			&& entry.z >= lhs[2] && entry.z <= rhs[2];
+	}
+
+}
